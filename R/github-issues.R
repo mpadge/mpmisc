3e4a41968b730294047ec7c296c7d996afc0ff3c
mpadge <mark.padgham@email.com>
2022-03-23 15:39:03 +0000

    Deploying to gh-pages from @ mpadge/mpmisc@10f9e7b16c28e2e5170f3eed848eb1488cdf582e 🚀

---
+++ R/github-issues.R
@@ -0,0 +1,148 @@
+
+# https://docs.github.com/en/graphql/reference/objects#user
+# https://docs.github.com/en/graphql/reference/objects#contributionscollection
+
+get_qry <- function (gh_cli, user = "mpadge", n = 20) {
+
+    # nolint start
+
+    q <- paste0 ("{
+        user(login:\"", user, "\") {
+             issueComments (last:", n, ") {
+                 edges {
+                     node {
+                         createdAt
+                         issue {
+                             number
+                             closed
+                             repository {
+                                 name
+                                 nameWithOwner
+                             }
+                         }
+                     }
+                 }
+             }
+            }
+        }")
+
+    # nolint end
+
+    qry <- ghql::Query$new ()
+    qry$query ("user", q)
+
+    return (qry)
+}
+
+#' Get latest comments from GitHub issues
+#'
+#' @param user GitHub user name
+#' @param n Number of latest comments to fetch
+#' @param open If `TRUE`, only return comments from open issues, otherwise
+#' include closed issues too.
+#' @return `data.frame` of commit data
+#' @export
+latest_comments <- function (user = "mpadge", n = 20, open = TRUE) {
+
+    token <- Sys.getenv("GITHUB_TOKEN")
+    auth <- paste0 ("Bearer ", token)
+
+    gh_cli <- ghql::GraphqlClient$new (
+                       url = "https://api.github.com/graphql",
+                       headers = list (Authorization = auth)
+                        )
+
+    qry <- get_qry (user = user, n = n * 5)
+
+    dat <- gh_cli$exec(qry$queries$user) |>
+        jsonlite::fromJSON ()
+
+    out <- dat$data$user$issueComments$edges$node
+    out <- data.frame (createdAt = out$createdAt,
+                       name = out$issue$repository$name,
+                       nameWithOwner = out$issue$repository$nameWithOwner,
+                       number = out$issue$number,
+                       closed = out$issue$closed)
+
+    out$repo_issue <- paste0 (out$name, "#", out$number)
+    out$org_repo_issue <- paste0 (out$nameWithOwner, "#", out$number)
+
+    if (open)
+        out <- out [which (!out$closed), ]
+
+    out$createdAt <- strptime (out$createdAt, "%Y-%m-%dT%H:%M:%SZ") # nolint
+
+    out <- out [order (out$createdAt, decreasing = TRUE), ]
+
+    out <- out [which (!duplicated (out$org_repo_issue)), ]
+
+    out <- out [seq (n), ]
+
+    rownames (out) <- NULL
+
+    cache_comments (out)
+
+    return (out)
+}
+
+cache_comments_file <- function () {
+
+    cache_dir <- rappdirs::user_cache_dir ("mpmisc")
+    if (!dir.exists (cache_dir))
+        dir.create (cache_dir, recursive = TRUE)
+
+    file.path (cache_dir, "latest_gh_comments.Rds")
+}
+
+
+cache_comments <- function (x) {
+
+    saveRDS (x, cache_comments_file ())
+}
+
+retrieve_cached_comments <- function () {
+
+    readRDS (cache_comments_file ())
+}
+
+#' Open web browser to specified GitHub issue
+#'
+#' Open issue only with name of repo, or if no issue specified open main issues
+#' page of repo. This uses \link{latest_comments} to extract issues for
+#' specified user in order to associate repo name with the correct GitHub
+#' organization.
+#'
+#' @param repo Name of repo
+#' @param issue Optional number of issue
+#' @return Nothing
+#' @export
+open_gh_issue <- function (repo = NULL, issue = NULL, user = "mpadge") {
+
+    if (is.null (repo))
+        stop ("repo must be specified")
+    if (length (repo) > 1L)
+        stop ("only one repo may be specified")
+
+    cmts <- retrieve_cached_comments ()
+
+    if (!repo %in% cmts$name)
+        cmts <- latest_comments (user = user, open = FALSE)
+
+    cmts <- cmts [which (cmts$name == repo), ]
+
+    url <- paste0 ("https://github.com/",
+                   cmts$nameWithOwner [1],
+                   "/issues")
+
+    if (!is.null (issue)) {
+
+        if (!is.numeric (issue))
+            stop ("issue must be a number")
+        if (length (issue) > 1L)
+            stop ("issue must be a single number")
+
+        url <- paste0 (url, "/", as.integer (issue))
+    }
+
+    browseURL (url = url)
+}
---
+++ R/precommit.R
@@ -0,0 +1,128 @@
+#' add_precommit_hooks
+#'
+#' Add precommit hooks from the \pkg{precommit} package, and modify to include
+#' an additional hook to ensure package version is incremented on every commit
+#' (via the \link{increment_dev_version} function). This function can also be
+#' used to update a local '.pre-commit-config.yaml' file to reflect the latest
+#' version at the specified 'url'.
+#'
+#' @note This function must be run within a package directory, and requires the
+#' \pkg{precommit} package to be installed.
+#'
+#' @note The function downloads a pre-commit hook file in
+#' 'inst/precommit/description', and notes that this file should be moved to a
+#' more general location, and the '.pre-commit-hooks.yaml' file updated
+#' accordingly. It may also be necessary to change the permissions of the
+#' 'description' pre-commit hook, for example (on *nix systems) via 'sudo chmod
+#' u_x description'.
+#'
+#' @param url The location of a repository containing the desired
+#' '.pre-commit-hooks.yaml' file to be installed in the local repository.
+#' @param branch The branch of the remote repository from which the
+#' `.pre-commit-hooks.yaml` file should be copied.
+#' @param location Local location to store precommit hooks. Default is relative
+#' path within current directory, but it is advised to set an absolute path to a
+#' more general location (such as '~/bin/precommit').
+#'
+#' @export
+add_precommit_hooks <- function (url = "https://github.com/mpadge/mpmisc",
+                                 branch = gert::git_branch (),
+                                 location = "inst/precommit") {
+    requireNamespace ("precommit")
+    here <- here::here ()
+
+    if (!hooks_exist (here)) {
+        precommit::use_precommit ()
+    }
+
+    grab_local_hooks (url, branch, location, here)
+}
+
+hooks_exist <- function (here) {
+    lf <- list.files (here, all.files = TRUE)
+    any (grepl ("^\\.pre-commit-config.yaml", lf))
+}
+
+grab_local_hooks <- function (url, branch, location, here) {
+    u <- paste0 (gsub ("/$", "", url),
+                 "/raw/",
+                 branch,
+                 "/.pre-commit-config.yaml")
+    x <- httr::GET (u)
+    if (x$status_code != 200)
+        stop ("http status [", x$status_code, "]")
+
+    xt <- httr::content (x, as = "text", encoding = "UTF-8")
+    xt <- strsplit (xt, "\n") [[1]]
+
+    message ("here = [", here, "]")
+    xt <- sub_location (xt, location)
+
+    writeLines (xt, file.path (here, ".pre-commit-config.yaml"))
+
+    add_to_rbuildignore (here)
+
+    download_hook (url, branch, here)
+
+    check_hook_location (here)
+}
+
+sub_location <- function (x, location) {
+    loc_default <- "inst/precommit"
+    if (location != loc_default) {
+        i <- grep (loc_default, x)
+        x_start <- paste0 (strsplit (x [i], ":") [[1]] [1], ": ")
+        x [i] <- paste0 (x_start, location, "/description")
+    }
+    return (x)
+}
+
+add_to_rbuildignore <- function (here) {
+    f <- file.path (here, ".Rbuildignore")
+    rb <- NULL
+    if (file.exists (f))
+        rb <- readLines (f)
+
+    chk <- grepl ("\\^\\\\\\.pre-commit-config\\\\.yaml\\$", rb)
+    if (!any (chk))
+        rb <- c (rb, "^\\.pre-commit-config\\.yaml$")
+    writeLines (rb, con = f)
+}
+
+download_hook <- function (url, branch, here) {
+    fp <- file.path (here, "inst", "precommit")
+    if (!file.exists (fp))
+        dir.create (fp, recursive = TRUE)
+
+    f <- file.path (fp, "description")
+    if (!file.exists (f)) {
+        u <- paste0 (gsub ("/$", "", url),
+                     "/raw/",
+                     branch,
+                     "/inst/precommit/description")
+        x <- httr::GET (u)
+        if (x$status_code != 200)
+            stop ("http status [", x$status_code, "]")
+
+        xt <- httr::content (x, as = "text", encoding = "UTF-8")
+        xt <- strsplit (xt, "\n") [[1]]
+
+        writeLines (xt, f)
+    }
+}
+
+check_hook_location <- function (here) {
+    f <- file.path (here, ".pre-commit-config.yaml")
+    if (!file.exists (f))
+        stop (".pre-commit-config.yaml does not exist here")
+
+    x <- readLines (f)
+    i <- grep ("id: description version", x)
+    if (length (i) == 1) {
+        i <- i + grep ("entry:", x [i:length (x)]) - 1
+        xi <- gsub ("\\s+entry:\\s", "", x [i])
+        message ("Please modify line#", i, " of 'pre-commit-config.yaml' to\n",
+                 "specify location of 'description' precommit hook\n",
+                 "Current location: ", xi)
+    }
+}
