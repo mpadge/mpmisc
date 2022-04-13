462b0f3a17ccd4585c98bca42a1f70f05f3c09a9
mpadge <mark.padgham@email.com>
2022-04-13 11:43:59 +0200

    format screen output for gh_daily_commits fn

--- R/github-commits.R
+++ R/github-commits.R
@@ -20,6 +20,17 @@ gh_daily_commits <- function (quiet = FALSE) {
 
     commits <- x$items$commit
     dates <- as.Date (strftime (commits$committer$date, "%Y-%m-%d"))
+    num_commits = length (which (dates == Sys.Date ()))
 
-    return (length (which (dates == Sys.Date ())))
+    # screen output stuff:
+    NC <- "\033[0m"                                            # nolint
+    ARG <- "\033[0;31m" # red                                  # nolint
+    TXT <- "\033[0;32m" # green, or 1;32m for light green      # nolint
+    SYM <- "\u2192" # right arrow                              # nolint
+
+    msg <- paste0 (SYM, " ", ARG, num_commits,
+                   TXT, " daily commits (",
+                   Sys.Date (), ")", NC)
+
+    message (msg)
 }
