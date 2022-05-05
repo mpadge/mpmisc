1c84532b98252d2d4eb32d99271b81ebc54a8fd0
mpadge <mark.padgham@email.com>
2022-05-05 15:05:32 +0200

    update gh_contributions to also output weekday

--- R/github-contributions.R
+++ R/github-contributions.R
@@ -1,11 +1,12 @@
 
-#' Count GitHub contributions for current day
+#' Count GitHub contributions for previous range of days.
 #'
 #' @param quiet If `FALSE`, print notifications to screen
-#' @param ndays Number of preceding days for which contributions should be extracted.
+#' @param ndays Number of preceding days for which contributions should be
+#' extracted.
 #' @return `data.frame` of calendar dates and contribution counts.
 #' @export
-gh_contributions <- function (quiet = FALSE, ndays = 5L) {
+gh_contributions <- function (quiet = FALSE, ndays = 7L) {
 
     q <- paste0 ("{
         user(login:\"mpadge\") {
@@ -22,28 +23,32 @@ gh_contributions <- function (quiet = FALSE, ndays = 5L) {
                              date
                              weekday
                          }
-                     } 
+                     }
                  }
              }
         }
     }")
 
     qry <- ghql::Query$new()
-    qry$query('user', q)
+    qry$query("user", q)
 
     token <- Sys.getenv("GITHUB_TOKEN") # or whatever
     gh_cli <- ghql::GraphqlClient$new (
-                       url = "https://api.github.com/graphql",
-                       headers = list (Authorization = paste0 ("Bearer ", token))
-                        )
+        url = "https://api.github.com/graphql",
+        headers = list (Authorization = paste0 ("Bearer ", token))
+    )
     dat <- gh_cli$exec(qry$queries$user) |>
         jsonlite::fromJSON ()
 
     cc <- dat$data$user$contributionsCollection
-    annual_total <- cc$contributionCalendar$totalContributions
+
+    #annual_total <- cc$contributionCalendar$totalContributions
     calendar <- do.call (rbind, cc$contributionCalendar$weeks$contributionDays)
     calendar$date <- as.Date (calendar$date)
-    calendar <- calendar [order (calendar$date, decreasing = TRUE), ]
+    calendar <- calendar [order (calendar$date, decreasing = TRUE), 1:2]
+    calendar$weekday <- as.character (lubridate::wday (calendar$date,
+                                                       label = TRUE,
+                                                       abbr = TRUE))
 
     # screen output stuff:
     NC <- "\033[0m"                                            # nolint
@@ -60,8 +65,9 @@ gh_contributions <- function (quiet = FALSE, ndays = 5L) {
     } else {
         message (TXT, "----", SYM, " Daily numbers of contributions:", NC)
         for (i in seq (ndays)) {
-            message ("   ", ARG, calendar$date [i],
-                     "  ", TXT, calendar$contributionCount [i])
+            message ("   ", ARG, calendar$weekday [i],
+                     "  ", calendar$date [i], " ",
+                     TXT, calendar$contributionCount [i])
         }
     }
 
