e6b6f0e393f7cf46cc13c2e66af8041fa9147cf4
mpadge <mark.padgham@email.com>
2023-06-27 10:47:11 +0200

    add win-builder.R

---
+++ R/win-builder.R
@@ -0,0 +1,33 @@
+#' Upload tarball to win-builder checks
+#'
+#' Code adapted from devtools/R/check-win.R
+#' @export
+win_builder_checks <- function () {
+
+    requireNamespace ("curl")
+    requireNamespace ("pkgbuild")
+
+    here <- here::here ()
+    setwd (here)
+
+    build_dir <- normalizePath (file.path (here, ".."))
+    path_tar <- pkgbuild::build (here, build_dir)
+    path_tar_file <- basename (path_tar)
+
+    url_base <- "ftp://win-builder.r-project.org/"
+    versions <- c ("R-oldrelease", "R-release", "R-devel")
+
+    for (v in versions) {
+
+        url <- paste0 (url_base, v, "/", path_tar_file)
+        con <- file (path_tar, open = "rb")
+        on.exit (close (con), add = TRUE)
+        h <- curl::new_handle (upload = TRUE, filetime = FALSE)
+        curl::handle_setopt (h, readfunction = function (n) {
+            readBin (con, raw (), n = n)
+        }, verbose = FALSE)
+        out <- curl::curl_fetch_memory (url, handle = h)
+
+        message ("Uploaded ", v)
+    }
+}
