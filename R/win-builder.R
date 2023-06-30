63986e60a5f827dee44e4bc4eb186a2c33623aec
mpadge <mark.padgham@email.com>
2023-06-30 14:21:17 +0200

    fix win-builder fn

--- R/win-builder.R
+++ R/win-builder.R
@@ -11,10 +11,9 @@ win_builder_checks <- function () {
     setwd (here)
 
     build_dir <- normalizePath (file.path (here, ".."))
-    path_tar_file <- file.path (build_dir, tar_from_desc (here))
-    if (!file.exists (path_tar_file)) {
+    path_tar <- file.path (build_dir, tar_from_desc (here))
+    if (!file.exists (path_tar)) {
         path_tar <- pkgbuild::build (here, build_dir)
-        path_tar_file <- basename (path_tar)
     }
 
     url_base <- "ftp://win-builder.r-project.org/"
@@ -22,7 +21,7 @@ win_builder_checks <- function () {
 
     for (v in versions) {
 
-        url <- paste0 (url_base, v, "/", path_tar_file)
+        url <- paste0 (url_base, v, "/", path_tar)
         con <- file (path_tar, open = "rb")
         h <- curl::new_handle (upload = TRUE, filetime = FALSE)
         curl::handle_setopt (h, readfunction = function (n) {
